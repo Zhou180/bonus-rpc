2e36dbfd88fdaad95da6e9a5cfbb8a401a2f9d4e
Zhou180 <sam8811288.cs08@nctu.edu.tw>
2022-12-29 17:13:06 +0800

    final version

--- build.sh
+++ build.sh
@@ -1,25 +1,22 @@
 # Build the server image
-docker build -t bonus_server_x86 -f Dockerfile.server .
+docker build -t bonus_server_arm -f Dockerfile.server .
 
 # Build the client image
-docker build -t bonus_client_x86 -f Dockerfile.client .
-
+docker build -t bonus_client_arm -f Dockerfile.client .
 
 # Run the server container
-docker run -d --name bonus_server_container bonus_server_x86
+docker run -d --name bonus_server_container --network host bonus_server_arm
 
 # Run the client container
-docker run -d --name bonus_client_container bonus_client_x86
-
+docker run -d -it --name bonus_client_container --network host bonus_client_arm
 
 # Create the network
-docker network create <network_name>
+# docker network create -d bridge rpc-bridge-network
 
 # Connect the server container to the network
-docker network connect <network_name> <server_container_name>
+# docker network connect rpc-bridge-network bonus_server_container
 
 # Connect the client container to the network
-docker network connect <network_name> <client_container_name>
+# docker network connect rpc-bridge-network bonus_client_container
 
-
-docker exec -it <client_container_name> python3 client.py -t testcase
+docker exec bonus_client_container python3 client.py 
--- client.py
+++ client.py
@@ -1,13 +1,12 @@
 import xmlrpc.client
 
-proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
+proxy = xmlrpc.client.ServerProxy("http://localhost:8000")
 
 testcases = [
     (5, 7, 12),
     (0, 0, 0),
     (-5, -7, -12),
     (100, 200, 300),
-    (1234567890, 9876543210, 11111111100)
 ]
 
 for test in testcases:
